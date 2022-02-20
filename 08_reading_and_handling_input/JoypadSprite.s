c82d20db600e32ec12a431d0455525530031dab9
Georg Ziegler <clc_xce@tutanota.com>
2022-02-20 13:51:28 +0100

    Remove code that isn't needed here

--- 08_reading_and_handling_input/JoypadSprite.s
+++ 08_reading_and_handling_input/JoypadSprite.s
@@ -76,99 +76,6 @@ ColorData:  .incbin "SpriteColors.pal"
         ldx #$1fff              ; load X with $1fff
         txs                     ; copy X to stack pointer
 
-        ; load sprites into VRAM
-        tsx                     ; save current stack pointer
-        pea $0000               ; push VRAM destination address to stack
-        pea SpriteData          ; push sprite source address to stack
-        lda #$80                ; push number of bytes (128/$80) to transfer to stack
-        pha
-        jsr LoadVRAM            ; transfer VRAM data
-        txs                     ; "delete" data on stack by restoring old stack pointer
-
-        ; load color data into CGRAM
-        tsx                     ; save current stack pointer
-        lda #$80                ; push CGRAM destination address to stack
-        pha
-        pea ColorData           ; push color source address to stack
-        lda #$20                ; push number of bytes (32/$20) to transfer to stack
-        pha
-        jsr LoadCGRAM           ; transfer color data into CGRAM
-        txs                     ; "delete" data on stack by restoring old stack pointer
-
-        ; set up initial data in the OAMRAM mirror, use X as index
-        ldx #$00
-        ; upper-left sprite
-        lda #(SCREEN_RIGHT/2 - SPRITE_SIZE) ; sprite 1, horizontal position
-        sta OAMMIRROR, X
-        inx                                 ; increment index
-        lda #(SCREEN_BOTTOM/2 - SPRITE_SIZE); sprite 1, vertical position
-        sta OAMMIRROR, X
-        inx
-        lda #$00                            ; sprite 1, name
-        sta OAMMIRROR, X
-        inx
-        lda #$00                            ; no flip, palette 0
-        sta OAMMIRROR, X
-        inx
-        ; upper-right sprite
-        lda #(SCREEN_RIGHT/2)               ; sprite 2, horizontal position
-        sta OAMMIRROR, X
-        inx                                 ; increment index
-        lda #(SCREEN_BOTTOM/2 - SPRITE_SIZE); sprite 2, vertical position
-        sta OAMMIRROR, X
-        inx
-        lda #$01                            ; sprite 2, name
-        sta OAMMIRROR, X
-        inx
-        lda #$00                            ; no flip, palette 0
-        sta OAMMIRROR, X
-        inx
-        ; lower-left sprite
-        lda #(SCREEN_RIGHT/2 - SPRITE_SIZE) ; sprite 3, horizontal position
-        sta OAMMIRROR, X
-        inx                                 ; increment index
-        lda #(SCREEN_BOTTOM/2)              ; sprite 3, vertical position
-        sta OAMMIRROR, X
-        inx
-        lda #$02                            ; sprite 3, name
-        sta OAMMIRROR, X
-        inx
-        lda #$00                            ; no flip, palette 0
-        sta OAMMIRROR, X
-        inx
-        ; lower-right sprite
-        lda #(SCREEN_RIGHT/2)                ; sprite 4, horizontal position
-        sta OAMMIRROR, X
-        inx                                 ; increment index
-        lda #(SCREEN_BOTTOM/2)              ; sprite 4, vertical position
-        sta OAMMIRROR, X
-        inx
-        lda #$03                            ; sprite 4, name
-        sta OAMMIRROR, X
-        inx
-        lda #$00                            ; no flip, palette 0
-        sta OAMMIRROR, X
-        inx
-        ; move the other sprites off screen
-        lda #$ff                ; set the coordinates to (255, 255), which is off screen
-OAMLoop:
-        sta OAMMIRROR, X
-        inx
-        cpx #OAMMIRROR_SIZE
-        bne OAMLoop
-
-        ; correct extra OAM byte for first four sprites
-        ldx #$0200
-        lda #$00
-        sta OAMMIRROR, X
-
-        ; set initial horizontal and vertical speed
-        lda #SPRITE_SPEED
-        sta HOR_SPEED
-        sta VER_SPEED
-
-        ; .byte $42, $00          ; debugger breakpoint
-
         ; make Objects visible
         lda #$10
         sta TM
@@ -190,90 +97,6 @@ OAMLoop:
 .proc   GameLoop
         wai                     ; wait for NMI / V-Blank
 
-        .byte $42, $00          ; debugger breakpoint
-        ; Game Logic: Move the sprites
-        ; move sprite 1 horizontally
-        ; check collision left boundary
-        lda HOR_SPEED
-        bpl RightBoundaryCheck  ; if sprites are moving right, skip left boundary check
-        lda OAMMIRROR
-        clc
-        adc HOR_SPEED
-        bcs UpdateHorPosition
-            ; else, reposition sprite 1 to horizontal position to zero
-            stz OAMMIRROR           ; reposition sprite 1
-            bra InvertHorSpeed      ; invert the horizontal speed
-        ; check right boundary
-RightBoundaryCheck:
-        lda OAMMIRROR
-        clc
-        adc HOR_SPEED
-        cmp #(SCREEN_RIGHT - 2 * SPRITE_SIZE)
-        bcc UpdateHorPosition   ; if sprite 1 is two sprites-wide to the left of right boundary, no collision
-            ; else, reposition sprite 1 horizontally
-            lda #(SCREEN_RIGHT - 2 * SPRITE_SIZE)
-            sta OAMMIRROR           ; reposition sprite 1 horizontally
-            bra InvertHorSpeed      ; invert the horizontal speed
-UpdateHorPosition:
-        sta OAMMIRROR           ; if no collision happened, store new position
-        bra VerticalCheck       ; check vertical collision
-        ; invert the horizontal speed after bouncing of the left/right screen boundary
-InvertHorSpeed:
-        lda HOR_SPEED           ; load current horizontal speed
-        eor #$ff                ; flip all bits
-        clc                     ; add 1 to inverted speed
-        adc #$01
-        sta HOR_SPEED           ; store inverted speed
-
-        ; move sprite 1 vertically
-VerticalCheck:
-        ; check collision upper boundary
-        lda VER_SPEED
-        bpl CheckLowerBoundary  ; if sprites are moving down, skip upper boundary check
-        lda OAMMIRROR + $01
-        clc
-        adc VER_SPEED
-        bcs UpdateVerPosition
-            ; else, reposition sprite 1 to horizontal position to zero
-            stz OAMMIRROR + $01     ; reposition sprite 1
-            bra InvertVerSpeed      ; invert the horizontal speed
-        ; check lower boundary
-CheckLowerBoundary:
-        lda OAMMIRROR + $01
-        clc
-        adc VER_SPEED
-        cmp #(SCREEN_BOTTOM - 2 * SPRITE_SIZE)
-        bcc UpdateVerPosition
-            ; else, reposition sprite 1 horizontally
-            lda #(SCREEN_BOTTOM - 2 * SPRITE_SIZE)
-            sta OAMMIRROR + $01     ; reposition sprite 1 horizontally
-            bra InvertVerSpeed      ; invert the horizontal speed
-UpdateVerPosition:
-        sta OAMMIRROR + $01     ; if no collision happened, store new position
-        bra UpdateOtherSprites  ; check vertical collision
-        ; invert the horizontal speed after bouncing of the left/right screen boundary
-InvertVerSpeed:
-        lda VER_SPEED           ; load current horizontal speed
-        eor #$ff                ; flip all bits
-        clc                     ; add 1 to inverted speed
-        adc #$01
-        sta VER_SPEED           ; store inverted speed
-
-UpdateOtherSprites:
-        lda OAMMIRROR           ; get new horizontal position of sprite 1
-        sta OAMMIRROR + $08     ; update sprite 3
-        clc
-        adc #SPRITE_SIZE
-        sta OAMMIRROR + $04     ; update sprite 2
-        sta OAMMIRROR + $0c     ; update sprite 4
-        ; vertical position
-        lda OAMMIRROR + $01     ; get new horizontal position of sprite 1
-        sta OAMMIRROR + $05     ; update sprite 2
-        clc
-        adc #SPRITE_SIZE
-        sta OAMMIRROR + $09     ; update sprite 3
-        sta OAMMIRROR + $0d     ; update sprite 4
-
         jmp GameLoop
 .endproc
 ;-------------------------------------------------------------------------------
@@ -284,7 +107,6 @@ UpdateOtherSprites:
 .proc   NMIHandler
         lda RDNMI               ; read NMI status, acknowledge NMI
 
-        ; this is where we would do graphics update
         tsx                     ; save old stack pointer
         pea OAMMIRROR           ; push mirror address to stack
         jsr UpdateOAMRAM        ; update OAMRAM
